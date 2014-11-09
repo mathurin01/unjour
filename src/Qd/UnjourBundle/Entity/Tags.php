d364c290536239025faf81b129cc213562192d42
mathurin <jpetiteau@gmail.com>
2014-11-09 23:07:53 +0100

    Création de entity Tags

---
+++ src/Qd/UnjourBundle/Entity/Tags.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace Qd\UnjourBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Tags
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Qd\UnjourBundle\Entity\TagsRepository")
+ */
+class Tags
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nom", type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="logo", type="string", length=255)
+     */
+    private $logo;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="indice", type="integer")
+     */
+    private $indice;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nom
+     *
+     * @param string $nom
+     * @return Tags
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get nom
+     *
+     * @return string 
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Set logo
+     *
+     * @param string $logo
+     * @return Tags
+     */
+    public function setLogo($logo)
+    {
+        $this->logo = $logo;
+
+        return $this;
+    }
+
+    /**
+     * Get logo
+     *
+     * @return string 
+     */
+    public function getLogo()
+    {
+        return $this->logo;
+    }
+
+    /**
+     * Set indice
+     *
+     * @param integer $indice
+     * @return Tags
+     */
+    public function setIndice($indice)
+    {
+        $this->indice = $indice;
+
+        return $this;
+    }
+
+    /**
+     * Get indice
+     *
+     * @return integer 
+     */
+    public function getIndice()
+    {
+        return $this->indice;
+    }
+}
