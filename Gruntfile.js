e78b3894c235d7c4f47aad6e1d933ec3352c3f30
mathurin <jpetiteau@gmail.com>
2014-12-05 08:22:38 +0100

    Ajout du minify d'images

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,11 +13,23 @@ module.exports = function(grunt){
                 bin: 'C:/xampp/php/phpcs',
                 standard: 'PSR2'
             }
+        },
+
+        imagemin: {
+            dist: {                         // Another target
+                files: [{
+                    expand: true,                  // Enable dynamic expansion
+                    cwd: 'web/bundles/qdunjour/images/',                   // Src matches are relative to this path
+                    src: ['**/*.{png,jpg,gif}'],   // Actual patterns to match
+                    dest: 'web/bundles/qdunjour/imagesmin/'                  // Destination path prefix
+                }]
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-phpcs');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
 
-    grunt.registerTask('default', ['jshint']);
+    grunt.registerTask('default', ['jshint', 'imagemin']);
 }
